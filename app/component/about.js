40925ef1d46a51e9cd73ebb72a93e707194071ad
0xdblok <dblok0x@gmail.com>
2024-04-18 10:00:48 +0200

    ee

--- app/component/about.js
+++ app/component/about.js
@@ -17,16 +17,16 @@ export default function About() {
       <h1 className="lg:text-4xl bg-clip-text text-transparent bg-gradient-to-b from-[#ffd29d] to-neutral-400   font-extrabold font-sans text-2xl text-center  pt-20 ">
         TOKENOMICS
       </h1>
-      <h1 className="rotate-180 c bg-clip-text text-transparent bg-gradient-to-b from-[#ffd29d] to-neutral-400 lg:text-4xl opacity-15 l font-extrabold font-sans text-2xl text-center  ">
+      <h1 className="rotate-180 c bg-clip-text text-transparent bg-gradient-to-b from-[#ffd29d] to-neutral-400 lg:text-4xl opacity-20 l font-extrabold font-sans text-2xl text-center  ">
         TOKENOMICS
       </h1>
       <div className="pt-8 px-12 flex justify-around">
         <div>
           {" "}
           <h1>TOTAL SUPPLY : 1B</h1>
-          <h1>TOTAL SUPPLY : 1B</h1>
-          <h1>TOTAL SUPPLY : 1B</h1>
-          <h1>TOTAL SUPPLY : 1B</h1>
+          <h1 className="pt-2">DEV 5%</h1>
+          <h1>MARKETING 15%</h1>
+          <h1>LP LOCK 80%</h1>
         </div>
         <div>
           {" "}
