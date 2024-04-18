ff19add5cbe07e37df1fdded24344476afd1f543
0xdblok <dblok0x@gmail.com>
2024-04-18 10:09:21 +0200

    ee

--- app/component/buy.js
+++ app/component/buy.js
@@ -10,7 +10,6 @@ export default function Buy() {
         </h1>
         <div className="lg:flex lg:gap-8 space-y-8 lg:space-y-0 pt-4 font-medium ">
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
-            Image
             <h1 className="text-center font-semibold">Create a Wallet</h1>
             <p className="text-sm text-center">
               Download Metamask wallet from the app store or google play store
@@ -19,7 +18,6 @@ export default function Buy() {
             </p>
           </div>
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
-            Image
             <h1 className="text-center font-semibold">Get Some ETH</h1>
             <p className="text-sm text-center break-words">
               Have ETH in your wallet to switch to $AAPL. If you don’t have any
@@ -28,7 +26,6 @@ export default function Buy() {
             </p>
           </div>
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
-            Image
             <h1 className="text-center font-semibold">Go to SyncSwap</h1>
             <p className="text-sm text-center">
               Google syncswap.xyz website. Connect your wallet. Paste the $AAPL
@@ -36,7 +33,6 @@ export default function Buy() {
             </p>
           </div>
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4 ">
-            Image
             <h1 className="text-center font-semibold">Ride the wave</h1>
             <p className="text-sm text-center">
               ZkSync Season is at its early stages. Just sit back and relax. The
