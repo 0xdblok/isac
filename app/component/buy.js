24c668f65f3daf4250a73a6aa972478be2067c61
0xdblok <dblok0x@gmail.com>
2024-04-19 10:53:01 +0200

    ee

--- app/component/buy.js
+++ app/component/buy.js
@@ -4,7 +4,7 @@ export default function Buy() {
   return (
     <div>
       {" "}
-      <div className="pt-36 font-sans pb-8">
+      <div className="pt-36 font-sans pb-16">
         <h1 className="lg:text-4xl font-extrabold font-sans text-2xl text-center bg-clip-text text-transparent bg-gradient-to-b from-[#ffd29d] to-neutral-400 ">
           HOW TO BUY
         </h1>
@@ -21,21 +21,21 @@ export default function Buy() {
             <h1 className="text-center font-semibold">Get Some ETH</h1>
             <p className="text-sm text-center break-words">
               Have ETH in your wallet to switch to $AAPL. If you don’t have any
-              ETH, you can buy ETH from an exchange and send it to ZkSync or
-              cross chain swap here https://zkswap.finance/zkbridge/bridge
+              ETH, you can buy ETH from an exchange and send it to Base or cross
+              chain swap here https://zkswap.finance/zkbridge/bridge
             </p>
           </div>
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
             <h1 className="text-center font-semibold">Go to SyncSwap</h1>
             <p className="text-sm text-center">
-              Google syncswap.xyz website. Connect your wallet. Paste the $AAPL
-              token address into SyncSwap and then confirm the swap. Then sign.
+              Google uniswap.org website. Connect your wallet. Paste the $AAPL
+              token address into Uniswap and then confirm the swap. Then sign.
             </p>
           </div>
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4 ">
             <h1 className="text-center font-semibold">Ride the wave</h1>
             <p className="text-sm text-center">
-              ZkSync Season is at its early stages. Just sit back and relax. The
+              Base Season is at its early stages. Just sit back and relax. The
               worst thing most people do, is they sell their bags early
             </p>
           </div>
