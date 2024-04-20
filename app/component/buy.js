a23c2694be70541098bbc13d61fbeb0c2640e729
0xdblok <dblok0x@gmail.com>
2024-04-20 20:51:30 +0200

    ee

--- app/component/buy.js
+++ app/component/buy.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+import Image from "next/image";
+import wallet from "../asset/wallet.png";
+import wall from "../asset/wall.png";
+import uni from "../asset/uni.png";
+import wave from "../asset/wave.png";
 export default function Buy() {
   return (
     <div>
@@ -10,6 +14,16 @@ export default function Buy() {
         </h1>
         <div className="lg:flex px-10 lg:gap-8 space-y-8 lg:space-y-0 pt-4 font-medium ">
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
+            <div className="flex justify-center">
+              {" "}
+              <Image
+                src={wall}
+                className=""
+                alt="Isaac Neewton"
+                width="150"
+                height="150"
+              />
+            </div>{" "}
             <h1 className="text-center font-semibold">Create a Wallet</h1>
             <p className="text-sm text-center">
               Download Metamask wallet from the app store or google play store
@@ -18,6 +32,16 @@ export default function Buy() {
             </p>
           </div>
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
+            <div className="flex justify-center">
+              {" "}
+              <Image
+                src={wallet}
+                className=""
+                alt="Isaac Neewton"
+                width="150"
+                height="150"
+              />
+            </div>{" "}
             <h1 className="text-center font-semibold">Get Some ETH</h1>
             <p className="text-sm text-center break-words">
               Have ETH in your wallet to switch to $. If you don’t have any ETH,
@@ -27,6 +51,16 @@ export default function Buy() {
             </p>
           </div>
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
+            <div className="flex justify-center">
+              {" "}
+              <Image
+                src={uni}
+                className=""
+                alt="Isaac Neewton"
+                width="150"
+                height="150"
+              />
+            </div>{" "}
             <h1 className="text-center font-semibold">Go to UniSwap</h1>
             <p className="text-sm text-center">
               Google uniswap.org website. Connect your wallet. Paste the $MOTION
@@ -34,6 +68,16 @@ export default function Buy() {
             </p>
           </div>
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4 ">
+            <div className="flex justify-center">
+              {" "}
+              <Image
+                src={wave}
+                className=""
+                alt="Isaac Neewton"
+                width="150"
+                height="150"
+              />
+            </div>
             <h1 className="text-center font-semibold">Ride the wave</h1>
             <p className="text-sm text-center">
               Base Season is at its early stages. Just sit back and relax. The
