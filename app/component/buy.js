b0d3c380c543c5d7c135cd5fb3c92cea55a05348
0xdblok <dblok0x@gmail.com>
2024-04-22 14:10:53 +0200

    ee

--- app/component/buy.js
+++ app/component/buy.js
@@ -26,9 +26,9 @@ export default function Buy() {
             </div>{" "}
             <h1 className="text-center font-semibold">Create a Wallet</h1>
             <p className="text-sm text-center">
-              Download Metamask wallet from the app store or google play store
+              Download Phantom wallet from the app store or google play store
               for free. Desktop users, download the google chrome extension by
-              going to metamask.io
+              going to phantom.app
             </p>
           </div>
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
@@ -42,12 +42,10 @@ export default function Buy() {
                 height="150"
               />
             </div>{" "}
-            <h1 className="text-center font-semibold">Get Some ETH</h1>
+            <h1 className="text-center font-semibold">Get Some SOL</h1>
             <p className="text-sm text-center break-words">
-              Have ETH in your wallet to switch to MOTION$. If you don’t have
-              any ETH, you can buy ETH from an exchange and send it to Base or
-              cross chain swap here
-              https://www.orbiter.finance/?source=Arbitrum&dest=Base&token=ETH
+              Have SOL in your wallet to switch to APPLE$. If you don’t have any
+              SOL, you can buy SOL from an exchange and send it to Solana.
             </p>
           </div>
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
@@ -61,10 +59,10 @@ export default function Buy() {
                 height="150"
               />
             </div>{" "}
-            <h1 className="text-center font-semibold">Go to UniSwap</h1>
+            <h1 className="text-center font-semibold">Go to Jupiter</h1>
             <p className="text-sm text-center">
-              Google uniswap.org website. Connect your wallet. Paste the $MOTION
-              token address into Uniswap and then confirm the swap. Then sign.
+              Google jup.ag website. Connect your wallet. Paste the $APPLE token
+              address into Jupiter and then confirm the swap. Then sign.
             </p>
           </div>
           <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4 ">
@@ -89,3 +87,86 @@ export default function Buy() {
     </div>
   );
 }
+
+/*
+<div>
+{" "}
+<div className="pt-36 font-sans pb-16">
+  <h1 className="lg:text-4xl font-extrabold font-sans text-2xl text-center bg-clip-text text-transparent bg-gradient-to-b from-[#ffd29d] to-neutral-400 ">
+    HOW TO BUY
+  </h1>
+  <div className="lg:flex px-10 lg:gap-8 space-y-8 lg:space-y-0 pt-4 font-medium ">
+    <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
+      <div className="flex justify-center">
+        {" "}
+        <Image
+          src={wall}
+          className=""
+          alt="Isaac Neewton"
+          width="150"
+          height="150"
+        />
+      </div>{" "}
+      <h1 className="text-center font-semibold">Create a Wallet</h1>
+      <p className="text-sm text-center">
+        Download Phantom wallet from the app store or google play store
+        for free. Desktop users, download the google chrome extension by
+        going to phantom.app
+      </p>
+    </div>
+    <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
+      <div className="flex justify-center">
+        {" "}
+        <Image
+          src={wallet}
+          className=""
+          alt="Isaac Neewton"
+          width="150"
+          height="150"
+        />
+      </div>{" "}
+      <h1 className="text-center font-semibold">Get Some SOL</h1>
+      <p className="text-sm text-center break-words">
+        Have ETH in your wallet to switch to MOTION$. If you don’t have
+        any ETH, you can buy ETH from an exchange and send it to Base or
+        cross chain swap here
+        https://www.orbiter.finance/?source=Arbitrum&dest=Base&token=ETH
+      </p>
+    </div>
+    <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4">
+      <div className="flex justify-center">
+        {" "}
+        <Image
+          src={uni}
+          className=""
+          alt="Isaac Neewton"
+          width="150"
+          height="150"
+        />
+      </div>{" "}
+      <h1 className="text-center font-semibold">Go to UniSwap</h1>
+      <p className="text-sm text-center">
+        Google uniswap.org website. Connect your wallet. Paste the $MOTION
+        token address into Uniswap and then confirm the swap. Then sign.
+      </p>
+    </div>
+    <div className="border-2 border-yellow-400 shadow-xl bg-[#ffd29d] p-8 rounded-lg lg:w-1/4 ">
+      <div className="flex justify-center">
+        {" "}
+        <Image
+          src={wave}
+          className=""
+          alt="Isaac Neewton"
+          width="150"
+          height="150"
+        />
+      </div>
+      <h1 className="text-center font-semibold">Ride the wave</h1>
+      <p className="text-sm text-center">
+        Base Season is at its early stages. Just sit back and relax. The
+        worst thing most people do, is they sell their bags early
+      </p>
+    </div>
+  </div>
+</div>
+</div>*/
