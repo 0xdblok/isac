cf6203bc662959eddafde96a3c5d84b48cf56142
0xdblok <dblok0x@gmail.com>
2024-04-24 16:43:43 +0200

    e

--- app/component/about.js
+++ app/component/about.js
@@ -26,9 +26,7 @@ export default function About() {
           <h1 className="text-center">TOTAL SUPPLY : 1B</h1>
           <h1 className="text-center">LP LOCK 100%</h1>
           <h1 className="text-center pt-12">CONTRACT ADDRESS</h1>
-          <h1 className="text-center">
-            9iyVZAHPjtdgdy3uw7BAjiUS4o3p6eT88k5avnVqen1A
-          </h1>
+          <h1 className="text-center">TBA</h1>
         </div>
       </div>
     </div>
